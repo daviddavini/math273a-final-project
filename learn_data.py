45bef7932acb374e4997b5848e3713793ce88671
David Davini <dndavini3.14@gmail.com>
2023-12-18 01:07:22 -0800

    epoch by epoch learn data

--- learn_data.py
+++ learn_data.py
@@ -1,6 +1,6 @@
 import torch
 from torch.nn.functional import mse_loss
-from matrices import line_matrix
+from matrices import line_matrix, sinusoid_matrix
 from net import random_conv_net
 from plots import plot_data, plot_loss, plot_weights
 from utils import save_constants, setup_save_dir
@@ -8,12 +8,13 @@ from utils import save_constants, setup_save_dir
 SAVE_DIR = "images/learn_data/latest"
 setup_save_dir(SAVE_DIR)
 
-LEARNING_RATE = 1e-2
-NUM_EPOCHS = 200
+LEARNING_RATE = 0.01
+NUM_EPOCHS = 2000
 NUM_DATA = 300
 NET_WIDTH = 100
-NET_DEPTH = 2
+NET_DEPTH = 5
 KERNEL_SIZE = 10
+EPOCH_BY_EPOCH = False
 
 save_constants({
     "LEARNING_RATE": LEARNING_RATE,
@@ -21,28 +22,31 @@ save_constants({
     "NUM_DATA": NUM_DATA,
     "NET_WIDTH": NET_WIDTH,
     "NET_DEPTH": NET_DEPTH,
-    "KERNEL_SIZE": KERNEL_SIZE
+    "KERNEL_SIZE": KERNEL_SIZE,
+    "EPOCH_BY_EPOCH": EPOCH_BY_EPOCH
 }, SAVE_DIR)
 
 net = random_conv_net(NET_WIDTH, KERNEL_SIZE, NET_DEPTH)
 for param in net.parameters():
     param.requires_grad = False
 
-# X = sinusoid_matrix(1, NUM_DATA, NET_WIDTH)
+X = sinusoid_matrix(1, NUM_DATA, NET_WIDTH)
 # X = torch.zeros(NUM_DATA, NET_WIDTH)
-X = torch.randn(NUM_DATA, NET_WIDTH) * 0.001
+# X = torch.randn(NUM_DATA, NET_WIDTH) * 0.001
 X.requires_grad = True
-# Y_preimage = torch.zeros(NUM_DATA, NET_WIDTH)
-Y_preimage = line_matrix(1, NUM_DATA, NET_WIDTH)
+Y_preimage = torch.zeros(NUM_DATA, NET_WIDTH)
+# Y_preimage = line_matrix(1, NUM_DATA, NET_WIDTH)
 Y = net(Y_preimage)
 optimizer = torch.optim.Adam([X], lr=LEARNING_RATE)
 
 plot_weights(net, "weights", None, SAVE_DIR)
 plot_data(Y, "Y", None, SAVE_DIR)
-plot_data(X, "X", "start", SAVE_DIR)
+if not EPOCH_BY_EPOCH:
+    plot_data(X, "X", "start", SAVE_DIR)
 losses = []
 for epoch in range(NUM_EPOCHS):
-    # plot_data(X, "X", "epoch_{}".format(epoch), SAVE_DIR)
+    if EPOCH_BY_EPOCH:
+        plot_data(X, "X", "epoch_{}".format(epoch), SAVE_DIR)
     optimizer.zero_grad()
     loss = mse_loss(net(X), Y)
     losses.append(loss.item())
@@ -51,4 +55,5 @@ for epoch in range(NUM_EPOCHS):
     print('Epoch: {}, Loss: {:.5e}'.format(epoch, loss))
 
 plot_loss(losses, SAVE_DIR)
-plot_data(X, "X", "end", SAVE_DIR)
+if not EPOCH_BY_EPOCH:
+    plot_data(X, "X", "end", SAVE_DIR)
